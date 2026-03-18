37deffe14e48b430809a48a99faa1b3e3d1432b1
Remy BORDE <rborde@sio.lan>
2026-03-18 15:36:06 +0100

    modif partie admin

--- AppCrmLourde/PageAdmin.xaml.cs
+++ AppCrmLourde/PageAdmin.xaml.cs
@@ -25,20 +25,6 @@ namespace AppCrmLourde
             InitializeComponent();
         }
 
-        private void BtnGestionProduits_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new PageGestionProduits()); // À créer
-        }
-
-        private void BtnGestionClients_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new PageGestionClients()); // À créer
-        }
-        private void BtnGestionFactures_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new PageGestionFactures()); // À créer
-        }
-
         private void BtnGestionManagers_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new PageGestionManagers()); // À créer
